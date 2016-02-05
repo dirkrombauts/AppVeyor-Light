7ccd5c9745848a60cd1479f818411157e5e40454
Dirk Rombauts <dirk.rombauts@questmaster.net>
2016-02-05 11:46:37 +0100

    Add first scenario

---
+++ ObjectModel.Specifications/ColorsOfBuildStates.feature
@@ -0,0 +1,13 @@
+﻿Feature: Colors Of Build States
+
+Scenario Outline: The colors that correspond to single build states
+
+    Given the build state is '<state>'
+    When I turn on the light
+    Then the light is '<color>'
+
+    Examples:
+        
+        | state   | color |
+        | Success | Green |
+
---
+++ ObjectModel.Specifications/ColorsOfBuildStatesSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+using NFluent;
+
+using TechTalk.SpecFlow;
+
+namespace AppVeyorLight.ObjectModel.Specifications
+{
+    [Binding]
+    public class ColorsOfBuildStatesSteps
+    {
+        private BuildResult buildState;
+
+        private Color actualColor;
+
+        [Given(@"the build state is '(.*)'")]
+        public void GivenTheBuildStateIs(string buildState)
+        {
+            this.buildState = (BuildResult)Enum.Parse(typeof(BuildResult), buildState);
+        }
+
+        [When(@"I turn on the light")]
+        public void WhenITurnOnTheLight()
+        {
+            this.actualColor = (Color)this.buildState;
+        }
+
+        [Then(@"the light is '(.*)'")]
+        public void ThenTheLightIs(string color)
+        {
+            Color expectedColor = (Color)Enum.Parse(typeof(Color), color);
+
+            Check.That(this.actualColor).IsEqualTo(expectedColor);
+        }
+    }
+}
